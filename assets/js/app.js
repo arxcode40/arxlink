a47a6c1ce3e717c94daa8ddc6321e70cee5aa596
arxcode40 <arxcode40@gmail.com>
2024-07-16 23:01:54 +0700

    Create scroll top button component

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,11 @@ function shareLink() {
 $(document).ready(function() {
 	$('#currentYear').text(new Date().getFullYear());
 })
+
+$(document).on('scroll', function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $('#scrollTop').fadeIn('fast');
+  } else {
+    $('#scrollTop').fadeOut('fast');
+  }
+})
