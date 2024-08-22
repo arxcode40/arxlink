344a9af5f4aee987728ac07416a7da66d121935b
arxcode40 <arxcode40@gmail.com>
2024-08-22 16:29:39 +0700

    Add sharer button, remake scroll to top button, and fixed any bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,15 @@
 "use strict";
 
-function shareLink() {
-	navigator.share({
-		title: 'ArX Link',
-		url: location.href
-	});
-}
-
 $(document).ready(function() {
-	$('#currentYear').text(new Date().getFullYear());
-})
+	new ClipboardJS("#clipboard");
 
-$(document).on('scroll', function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    $('#scrollTop').fadeIn('fast');
+	$("#currentYear").text(new Date().getFullYear());
+});
+
+$(document).on("scroll", function() {
+  if ($(document).scrollTop() > 20) {
+    $("#scrollToTop").fadeIn("fast");
   } else {
-    $('#scrollTop').fadeOut('fast');
+    $("#scrollToTop").fadeOut("fast");
   }
-})
+});
